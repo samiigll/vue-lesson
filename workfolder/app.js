d035e8cbc5c7fa955c7320f58fcd6e18a1321178
Sami GÜL <148059353+samiigll@users.noreply.github.com>
2024-02-11 10:42:03 +0300

    lesson 1

---
+++ workfolder/app.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      name: "Sami", //String
+      job: "Mühendis", //String
+      age: 25, //Number
+      giyim: {
+        sapka: true, //Boolean
+        pantolon: 'kot', //String
+        tshirt: false //Boolean
+      },
+      yetenekler: ['Yazılım', 'Tasarım', 'Yönetim'], //Array
+    };
+  },
+}).mount("#ilk-uygulama");
+ 
